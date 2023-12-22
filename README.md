eef9dd547988f3833b0aaaa9870065dfc5f4714d
tharanga270 <developerwordpress270@gmail.com>
2023-12-22 18:19:09 +0530

    9.project complete with daynamic

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ create responsive homepage using React,Boostrap 5,HTML5 and CSS3
 
 ## To run Project
 
-go to project folder and
+go to the project folder and
 
 ### `npm start`
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,29 @@
 import imghair1 from '../images/hair-cut1.jpg';
 import imghair2 from '../images/hair-cut2.jpg';
 import imghair3 from '../images/hair-cut3.jpg';
+
+const bannerInfo = {
+  title1: 'beard',
+  title2: 'cut',
+  btnName: 'BOOK NOW',
+  bannerContent:
+    'A short beard cut is a style of facial hari grooming where the beard is trimmed to a relatively short length, typically ranging from stubble to a few centimeters in length. Short beard styles are characterized by neatly trimmed, well-defined lines and minimal facial hair growth.',
+};
+const feachers = {
+  dot1Text: 'Hair',
+  dot2Text: 'Nails',
+  dot3Text: 'Beard',
+  dot4Text: 'Skin',
+};
+
 function Banner() {
   return (
     <>
       <section className="banner-wrapper">
-        <div style={{ border: '1px solid transparent' }} className="container px-0">
+        <div
+          style={{ border: '1px solid transparent' }}
+          className="container px-0"
+        >
           <div className="row" style={{ marginTop: '300px' }}>
             <div className="col-md-7 col7">
               <div style={{ position: 'relative' }}>
@@ -15,17 +33,11 @@ function Banner() {
               </div>
             </div>
             <div className="col-md-5 banner-right text-end">
-              <span className="banner-hd1">beard</span>
-              <span className="banner-hd2">cut</span>
+              <span className="banner-hd1">{bannerInfo.title1}</span>
+              <span className="banner-hd2">{bannerInfo.title2}</span>
               <br />
-              <button className="btn btn-booknow">BOOK NOW</button>
-              <p>
-                A short beard cut is a style of facial hari grooming where the
-                beard is trimmed to a relatively short length, typically ranging
-                from stubble to a few centimeters in length. Short beard styles
-                are characterized by neatly trimmed, well-defined lines and
-                minimal facial hair growth.
-              </p>
+              <button className="btn btn-booknow">{bannerInfo.btnName}</button>
+              <p>{bannerInfo.bannerContent}</p>
             </div>
           </div>
 
@@ -34,25 +46,25 @@ function Banner() {
               <div className="dots">
                 <i className="fa-solid fa-chair"></i>
               </div>
-              <span>Hair</span>
+              <span>{feachers.dot1Text}</span>
             </div>
             <div className="dots-wrapper">
               <div className="dots">
                 <i className="fa-regular fa-paper-plane"></i>
               </div>
-              <span>Nails</span>
+              <span>{feachers.dot2Text}</span>
             </div>
             <div className="dots-wrapper">
               <div className="dots">
-              <i className="fa-solid fa-palette"></i>
+                <i className="fa-solid fa-palette"></i>
               </div>
-              <span>Beard</span>
+              <span>{feachers.dot3Text}</span>
             </div>
             <div className="dots-wrapper">
               <div className="dots">
                 <i className="fa-solid fa-sink"></i>
               </div>
-              <span>Skin</span>
+              <span>{feachers.dot4Text}</span>
             </div>
           </div>
         </div>
